059710e769f665e0eb9cc3701c6043a17a516363
ZeeshanMazhar90 <zeeshan.mazhar798@gmail.com>
2022-12-29 23:27:32 +0500

    logo animation

--- src/screens/CurrencyConverterScreen.js
+++ src/screens/CurrencyConverterScreen.js
@@ -1,20 +1,55 @@
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
+import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Animated, Keyboard} from 'react-native'
+import React, {useEffect, useState} from 'react'
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 const CurrencyConverterScreen = ({navigation,route}) => {
     const [currency1, setCurrency1] = useState("");
     const [currency2, setCurrency2] = useState("");
+    const [logoAnimation] = useState(new Animated.Value(0));
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow',
+            () => {
+                Animated.timing(logoAnimation, {
+                    toValue: -45,
+                    duration: 1000,
+                    useNativeDriver: false,
+                }).start();
+            }
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide',
+            () => {
+                Animated.timing(logoAnimation, {
+                    toValue: 0,
+                    duration: 1000,
+                    useNativeDriver: false,
+                }).start();
+            }
+        );
+
+        return () => {
+            keyboardDidShowListener.remove();
+            keyboardDidHideListener.remove();
+        };
+    }, []);
+
+
 
     return (
         <View style={{ flex: 1, paddingTop: 40, backgroundColor: route?.params?.color==undefined?'#496e7b': route?.params?.color}}>
             <TouchableOpacity style={{ alignItems: 'flex-end', marginRight: 20 }} onPress={() => {
                     navigation.navigate('Options')
             }}>
-                <Image source={require('../assets/settings.png')} style={{ width: 20, height: 20, tintColor: 'white' }} />
+                <Image source={require('../assets/settings.png')} style={{ width: 25, height: 25, tintColor: 'white' }} />
             </TouchableOpacity>
             <View style={{ justifyContent: 'center', flex: 1 }}>
-                <Text style={{ alignSelf: 'center', marginBottom: 20, fontSize: 25, fontWeight: 'bold', color: 'white' }}>Currency Converter</Text>
-                <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
+                <Animated.View style={{alignItems:'center', transform: [{ translateY: logoAnimation }] }}>
+                    <Image style={{width:120,height:120}} tintColor={'#fff'} source={require('../assets/icons8-currency-exchange-100.png')} />
+                </Animated.View>
+                <Text style={{ alignSelf: 'center', marginTop: 50, fontSize: 25, fontWeight: 'bold', color: 'white' }}>Currency Converter</Text>
+                <View style={{ flexDirection: 'row', marginHorizontal: 20,marginTop: 20 }}>
                     <TouchableOpacity onPress={() => {navigation.navigate('BaseCurrency') }}>
                         <View style={styles.currencyNameView} >
                             <Text style={{color:route?.params?.color==undefined?'#496e7b': route?.params?.color,fontWeight:'bold',fontSize:18}}>USD</Text>
@@ -41,10 +76,13 @@ const CurrencyConverterScreen = ({navigation,route}) => {
                     />
                 </View>
                 <Text style={{ alignSelf: 'center', marginBottom: 20, fontSize: 15, color: 'white', marginTop: 20 }}>1 usd = 0.2323 as of Sep 23</Text>
-                <TouchableOpacity onPress={() => {
+                <TouchableOpacity
+                    style={{ marginTop: 20,flexDirection:'row',alignItems:'center',justifyContent:'center'}}
+                    onPress={() => {
                     // props.navigation.navigate('Base Currency')
                 }}>
-                    <Text style={{ alignSelf: 'center', marginBottom: 20, fontSize: 15, color: 'white', marginTop: 20 }}>Currency Converter</Text>
+                    <AntDesign name={"retweet"} color={"#fff"} size={22}/>
+                    <Text style={{ marginLeft: 15, fontSize: 15, color: 'white', }}>Reverse Currencies</Text>
                 </TouchableOpacity>
 
             </View>
